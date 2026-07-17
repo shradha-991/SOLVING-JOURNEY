6997eacdce5ccbb26a0333d8c49f66e2273d8e03
Shradha Garg <shradhagarg0909@gmail.com>
2026-07-17 15:24:11 +0530

    Update 0011-container-with-most-water.java

--- 0011-container-with-most-water/0011-container-with-most-water.java
+++ 0011-container-with-most-water/0011-container-with-most-water.java
@@ -1,19 +1,28 @@
-class Solution {
-    public int maxArea(int[] height) {
+import java.util.Scanner;
+class Solution{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int[] height=new int[n];
+        for(int i=0;i<n;i++){
+            height[i]=sc.nextInt();
+        }
         int left=0;
         int right=height.length-1;
         int maxArea=0;
         while(left<right){
             int h=Math.min(height[left],height[right]);
             int width=right-left;
-            int area=h*width;
-            maxArea=Math.max(area,maxArea);
+            int area=width*h;
+            maxArea=Math.max(maxArea,area);
             if(height[left]<height[right]){
                 left++;
-            }else{right--;}
-
-        }
-        return maxArea;
+            }else{
+                right--;
+            }
+        }System.out.println(maxArea);
+      
         
+        }
     }
-}
+
